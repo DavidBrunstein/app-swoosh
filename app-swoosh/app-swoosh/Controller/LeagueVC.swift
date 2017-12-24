4253c60b43e0d1b180e3a7d7ac097b235b6e325b
Davo Brunstein <davidbrunstein@gmail.com>
2017-12-24 14:57:41 -0600

    passing data between ViewControllers

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,14 +10,40 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!     // implicit unwrapped optional
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        player = Player()
     }
 
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+
+    func selectLeague(leagueType: String) {
+        player.desireLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
     }
 
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by David Brunstein on 2017-12-17.
+//  Copyright © 2017 David Brunstein. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desireLeague: String!
+    var selectedSkillLevel: String!
+}
